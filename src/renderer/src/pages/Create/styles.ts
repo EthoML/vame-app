dcf655ed76ae540085ed57957f1689a9c80dc07d
luiz <luiz_tauffer@hotmail.com>
2025-04-30 14:09:41 +0200

    create page

--- src/renderer/src/pages/Create/styles.ts
+++ src/renderer/src/pages/Create/styles.ts
@@ -6,4 +6,18 @@ export const PaddedContainer = styled.div`
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
+  position: relative;
+`;
+
+export const FormOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
 `;
